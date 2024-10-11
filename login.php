55b370592334568f01a120f257f802008070f0ff
Felipe Sloboda <felipe.ti.sloboda@gmail.com>
2024-10-11 16:26:03 -0300

    Ajuste com inclusao de PDO em login.pgp

--- login.php
+++ login.php
@@ -1,28 +1,24 @@
 <?php
 
 $usuario = $_POST["usuario"];
-$senha = md5($_POST["senha"]);
+$senha = $_POST["senha"];
 
-$connect = mysql_connect("servidor", "usuario", "senha");
-$bd = mysql_select_db("nomebanco");
-$select = "SELECT * FROM cadastros WHERE usuario = "$usuario" AND senha = "$senha"";
-
+$select = "SELECT * FROM cadastros WHERE USER = '$usuario' and PASS = '$senha'";
+print($select);
 try {
-$cadastros = mysql_query($select) or die("ERRO SELECT USUARIOS BD");
-    if(mysql_num_rows($cadastros)>0){
+    $pdo = new PDO("mysql:host=localhost; dbname=sistemaloginweb","root","");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = $pdo->prepare($select);
+    $sql->execute();
+    if($sql->rowCount() >= 1){
         setcookie("usuario",$usuario);
-        header("Location:index.php");
+        header('Location: index.php');
     }
     else{
-        echo"<script language="javascript" type="text/javascript">
-            alert("USUARIO OU SENHA INCORRETO");window.location.href="login.html";
-        </script>";
+        header('Location: login.html');
     }
 }
-catch (
-echo"<script language="javascript" type="text/javascript">
-    alert("ERRO AO EFETUAR LOGIN, TENTE NOVAMENTE MAIS TARDE");window.location.href="login.html";
-</script>";
-)
-
+catch (PDOException $e) {
+    die("ERRO AO CONECTAR COM SQL:" . $e->getMessage());
+}
 ?>
