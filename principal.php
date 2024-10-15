1d3b5c94e45b8b76b772d24adc1853fd9da1d3c8
FelipeSloboda <felipe.ti.sloboda@gmail.com>
2024-10-15 14:16:45 -0300

    Inclusao mensagem de boas vindas na tela principal.php
    Inclusao mensagem de boas vindas na tela principal.php

--- principal.php
+++ principal.php
@@ -1,7 +1,7 @@
 <?php
 $usuario_cookie = $_COOKIE["usuario"];
 if(isset($usuario_cookie)){
-    print($usuario_cookie);
+    echo("Bem vindo " . $usuario_cookie);
 }
 else{
     header('Location: login.html');
